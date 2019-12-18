d86b5f250be736b8fd6170599b9d9d76a2134b7a
Georgiy <egor_kinderr@mail.ru>
2019-12-18 10:40:54 +0300

    added a class LoginPage with three checks, added three test-case, added a class LoginPageLocators

---
+++ pages/login_page.py
@@ -0,0 +1,14 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_url(self):
+        url = self.browser.current_url
+        assert '/login/' in url, "The link is incorrect"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Register form is not presented"
